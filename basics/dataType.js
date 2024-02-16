9ed26fdb683d83c49baa3a5b29c9054fbc2663f4
abhisheksharma-github <153059659+abhisheksharma-github@users.noreply.github.com>
2024-02-16 15:40:45 +0000

    Data Types  In Javascript

---
+++ basics/dataType.js
@@ -0,0 +1,23 @@
+let str="Abhishek Sharma"
+let age=21
+let heISboy=true
+console.log(typeof str,age,heISboy)
+/*
+Null =  it shows a default Object
+Number=>2 to power 53
+Symbol= its shows the uniquness 
+String=> represent the character/name , use " " and ' '
+BigINT => greater the number
+Boolean = it is use for wheather the statment is TRUE or FALSE
+Undefined 
+*/
+
+let a= null
+let b =345
+let c= Symbol("I am Symbol")
+let d = "Abhi"
+let e = BigInt(1234567890123456789012345)
+let f= true
+let g = undefined
+console.table([a,b,c,d,e,f,g])
+console.log(typeof a )
