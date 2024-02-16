24681bcd0fc4c80b2359363e9f80c63f9d6f82fd
abhisheksharma-github <153059659+abhisheksharma-github@users.noreply.github.com>
2024-02-16 15:18:55 +0000

    variable in js

---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const studentId=1805 
+//isko hum change nahi kar skte  neither upadate nor re-declared
+
+let StudentName="Abhishek "
+// can't redeclared
+
+var StudentCourse="Btech "
+// isko change kar skte
+studentBranch= "ComputerScience"
+// we can declared like this also
+
+studentYearofGraduation=2021
+
+console.table([studentId,StudentName,StudentCourse,studentBranch,studentYearofGraduation]) 
+// hum isko table form mein bhi print karva skte ha 
+
+console.log(studentId)
