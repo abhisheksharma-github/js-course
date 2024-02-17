8bd2551b6897ded4d5332cc6c5bdf8d8ef48924a
abhisheksharma-github <153059659+abhisheksharma-github@users.noreply.github.com>
2024-02-17 18:13:33 +0000

    comparsion in js

---
+++ basics/comparsion.js
@@ -0,0 +1,18 @@
+/*console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2<=1)
+console.log(2!1)*/
+
+console.log("2" > 1)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+// equality check and comparsion work differently ,comparsion convert null to number as 0 ,,isliye (3) null >0 true and (1) null>0 false
+
+// === triple equal is to check wheather the datatype is equal ya same value h ki nahi
+
+console.log("2"==="2");
+console.log("2"==="abhi")
