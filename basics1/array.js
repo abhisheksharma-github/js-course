87685ddcc892dcb3d5a2a12385676b29b7ca5600
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-03-15 13:00:50 +0000

    ARRAY BASICS

---
+++ basics1/array.js
@@ -0,0 +1,14 @@
+/*- JavaScript arrays:
+
+  - Resizable, accommodate various data types.
+  - For stricter typing, consider typed arrays.
+- Not associative:
+  - Accessed via non-negative integers.
+- Zero-indexed:
+  - First element: index 0.
+  - Last element: length minus 1.
+- Copy operations:
+  - Shallow copies made, not deep copies.*/
+
+  const arr=["1",2,3,4]
+console.log(arr[0]);
