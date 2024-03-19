4bd64c93efe83fee9cfa049e98237bb589daeb86
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-03-19 17:45:32 +0000

    Object In JS

--- basics1/object.js
+++ basics1/object.js
@@ -6,5 +6,28 @@ const Student = {
     email:"abhishek@google.com",
     [mysum]: "mykey1"
 }
-// thats how we declare the object
+// thats how we access the object most of time
+console.log(Student.email);
+// we use sqr bracket for sometimes 
 console.log(Student[mysum]);
+
+// TO CHANGE ANYTHING IN OBJECY WE USE THIS METHOD 
+ // if you want nobody change anything we use .freeze method
+Student.email="abhisheksharma@balele.com"
+console.log(Student);
+  // if you want nobody change anything we use .freeze method
+    // Object.freeze(Student)
+Student.email="abhishek@gmail.com"
+
+// we can create Function in the Object like this
+
+Student.wish = function(){
+    console.log("Hello Students, How Are You?")
+}
+console.log(Student.wish) // if we use this it show it is a FUNCTION 
+console.log(Student.wish())// its shows the content of Function in It
+
+Student.wishTwo = function(){
+    console.log(`Hello,${this.name} ,Your Age is ${this.age}`)
+}
+console.log(Student.wishTwo());
