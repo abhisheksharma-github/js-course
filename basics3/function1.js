709993a642318236a0e2b8585c3de6d7e745ccc2
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-04-01 06:53:50 +0000

    #

---
+++ basics3/function1.js
@@ -0,0 +1,13 @@
+
+function myName(){
+    console.log("ABHISHEK SHARMA");
+}
+
+function addNumber(number1,number2)
+{
+ return number1+ number2;  // return ke baad kuch bhi print nahi hoga 
+}
+
+const result = addNumber(3,4)
+console.log("Result of Add Number is",result)
+
