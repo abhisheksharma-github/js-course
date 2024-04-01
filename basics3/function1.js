dfed2ec9b0eb060bcf2cf61c406a64d8ae0315eb
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-04-01 10:01:18 +0000

    scope

--- basics3/function1.js
+++ basics3/function1.js
@@ -1,5 +1,5 @@
 function myName() {
-    console.log("ABHISHEK SHARMA");
+    // console.log("ABHISHEK SHARMA");
 }
 
 function addNumber(number1, number2) {
@@ -7,4 +7,33 @@ function addNumber(number1, number2) {
 }
 
 const result = addNumber(3, 4);
-console.log(result);
+// console.log(result);
+
+
+function Cart(...name){
+    return name;
+}
+console.log(Cart('soap,cream,tube,doll'))
+
+const user = {
+    username:"abhi",
+    price:99
+}
+
+function handleUser(anyobject){
+    // console.log(`Username ${anyobject.username} price is ${anyobject.price}`)
+}
+handleUser(user);
+
+
+
+function Father(){
+   const FatherName="mukesh"
+   function Son(){
+    const sonName="abhishek"
+    console.log(FatherName)
+   }
+   Son()
+}
+Father()
+
