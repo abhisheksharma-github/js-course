6d735ec0681b7ce10f3155fd5ca8826c006ef410
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-04-01 07:09:02 +0000

    Function

--- basics3/function1.js
+++ basics3/function1.js
@@ -1,13 +1,10 @@
-
-function myName(){
+function myName() {
     console.log("ABHISHEK SHARMA");
 }
 
-function addNumber(number1,number2)
-{
- return number1+ number2;  // return ke baad kuch bhi print nahi hoga 
+function addNumber(number1, number2) {
+    return ` Addition of ${number1} + ${number2} is = ${number1 + number2}`;   // return ke baad kuch bhi print nahi hoga 
 }
 
-const result = addNumber(3,4)
-console.log("Result of Add Number is",result)
-
+const result = addNumber(3, 4);
+console.log(result);
