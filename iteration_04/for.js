7504c3abd970bce65d7eb2e3b560ac91d6cec344
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-04-04 11:43:24 +0000

    for loop

---
+++ iteration_04/for.js
@@ -0,0 +1,18 @@
+// for loop
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Outer loop is :${i}`);
+//     for (let j = 1; j <=10; j++) {
+//         console.log(`${i}*${j} = ${i * j}`);
+
+//     }
+    
+// }
+
+for (let index = 1; index <=20; index++) {
+    if(index==5){
+        console.log(` Founded:${index}`);
+        break;
+    }
+    console.log(index);
+}
