32b99bf50227d78cbc0447ebe7da728de9f8b602
Abhishek Sharma <153059659+abhisheksharma-github@users.noreply.github.com>
2024-04-04 06:15:14 +0000

    iife in js

---
+++ basics3/iife.js
@@ -0,0 +1,9 @@
+// this called named iife 
+(function chai(){
+    console.log(`Github is Connected`)
+})();
+
+// this is called simple iife
+((name)=>{
+    console.log(`the name of this person is ,${name}`)
+})("abhishek")
